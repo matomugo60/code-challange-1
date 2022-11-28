187166ff6d9550398f9814de7f8337c2803ea035
matomugo60 <matomugo60@gmail.com>
2022-11-28 09:02:02 +0300

    Second Commit

--- index.js
+++ index.js
@@ -30,54 +30,38 @@ let result = gradeCalculator (54, 100)
 console.log (result)
 
 
-// Defining variable speedDetector and setting it as a function for argument speed which has a value of 70 and demerit.
+// Defining variable carSpeedDetector and setting it as a function for argument carSpeed 
 
-let speedDetector = function (speed = 70, demerit) {
-    // Setting variable carSpeed to contain speed and setting the value of demerit.
-    let carSpeed = speed
-    demerit = " "
-// Using else-if statements to return different outcomes.
+function carSpeedDetector(carSpeed) {
+    const speedLimit = 70
+    const speedRate = 5
 
+// If statement to print Ok if carspeed is below 70
     if (carSpeed <= 70) {
-        demerit = "Ok"
-    } else if (carSpeed >= 75) {
-        demerit = "Points : 1"
-    } else if (carSpeed >= 85) {
-        demerit = "Points : 2"
-    } else if (carSpeed >= 90) {
-        demerit = "Points : 3"
-    } else if (carSpeed >= 95) {
-        demerit = "Points : 4"
-    } else if (carSpeed >= 100) {
-        demerit = "Points : 5"
-    } else if (carSpeed >= 105) {
-        demerit = "Points : 6"
-    } else if (carSpeed >= 110) {
-        demerit = "Points : 7"
-    } else if (carSpeed >= 115) {
-        demerit = "Points : 8"
-    } else if (carSpeed >= 120) {
-        demerit = "Points : 9"
-    } else if (carSpeed >= 125) {
-        demerit = "Points : 10"
-    } else if (carSpeed >= 130) {
-        demerit = "Points : 11"
-    } else if (carSpeed >= 135) {
-        demerit = "Points : 12"
-    } else {
-        demerit = "License suspended"
+        return "OK"
     }
-    // A return message indicating the speed and value of demerit
-    return 'Speed :'  +  speed  + '--' + 'Status--'  +  demerit
+// Variable declaration for excessCarSpeed to be used in the function declaration that prints License Suspended
+    const excessCarSpeed = carSpeed - speedLimit;
+
+    let demeritPoints = 0;
+    //Using for statement to set values to be used in excessSpeed
+    for( let i=0; i < excessSpeed; i+= speedRate ) {
+        demeritPoints += 1
+        // If statement to return License Suspended using demeritPoints > 12
+        if(demeritPoints > 12) {
+            return "Licence Suspended"; 
+        }
+    }
+     // A return message indicating the Points value
+    return `Points: ${demeritPoints}`;
 }
-// Using variable movement to call function speedDetector and using 91 as the speed covered.
-let movement = speedDetector(77)
+
 // Using console.log to print the result
-console.log(movement)
+console.log (carSpeedDetector (57));
 
 
 // Defining variable grossSalary and setting it as a function for payCheck and totalDeductions
-var grossSalary = function (payCheckMonthly = 24000, totalDeductions ) {
+var grossSalary = function (payCheckMonthly = 24000 - totalDeductions ) {
     var totalDeductions = deductionNSSF + deductionKRA + deductionNHIF
     var deductionNSSF = (payCheckMonthly * 6) / 100
     var deductionKRA = (payCheckMonthly * 10) / 100
@@ -103,3 +87,16 @@ var grossSalary = function (payCheckMonthly = 24000, totalDeductions ) {
 let netSalary = grossSalary (26700)
 // Printing the result 
 console.log(netSalary)
+
+
+
+
+
+
+
+
+
+
+
+
+
